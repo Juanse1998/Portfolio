13365a33ad94522cdf4b4218199aacd573227b18
Juan Sosa <jsosa@wisboo.com>
2024-01-11 17:19:27 -0300

    add responsive mobile

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -4,22 +4,20 @@ import "./Education.css";
 export default function Education() {
   return (
     <>
-      <span className="titleEducation" id="education">
-        Educación
-      </span>
-      <div className="xxi">
-        <div className="educationContent">
-          <span className="education">Universidad Siglo XXI-</span><span className="career">Licenciatura en informática</span>
-        </div>
-        <div>
+      <div className="education">
+        <span className="titleEducation" id="education">
+          Educación
+        </span>
+        <div className="school">
+          <span className="nameSchool">Universidad Siglo XXI</span><span className="careerXXI">Licenciatura en informática</span>
           <a className="descriptionEducation">Actualmente me encuentro cursando la carrera de Licenciatura en informática en la Universidad Siglo XXI</a>
         </div>
-      </div>
-      <div className="henry">
-          <span className="education">Soy Henry-</span><span className="career">Fullstack developer</span>
-      </div>
-      <div>
-        <a className="descriptionEducation">Junio 2021 - Abril 2022: Desarrollo de nuevas funcionalidades ymantenimiento de aplicación web tanto en frontend como en backend utilizando Ruby on Rails, AngularJs, Javascript.</a>
+        <div className="bootcamp">
+            <span className="nameBootcamp">Soy Henry</span><span className="career">Fullstack developer</span>
+        </div>
+        <div>
+          <a className="descriptionEducation">Junio 2021 - Abril 2022: Desarrollo de nuevas funcionalidades y mantenimiento de aplicación web tanto en frontend como en backend utilizando Ruby on Rails, AngularJs, Javascript.</a>
+        </div>
       </div>
     </>
   );
